d31984f860fd56f3ca1182395ae38353c4d3974e
Virture <Virtue@VirtuedeMacBook-Pro-2.local>
2019-08-16 16:56:00 +0800

    commit

---
+++ CodeObfuscationDemo/CodeObfuscationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeObfuscationDemo
+//
+//  Created by Virtue on 2019/8/16.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
